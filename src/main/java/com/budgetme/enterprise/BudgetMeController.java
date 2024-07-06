80b65487b55cbd1b8c5b9c04fc0ad541b24b1467
danny <40042446+xxxlaf@users.noreply.github.com>
2024-07-06 10:41:11 -0400

    Added JavaDoc documentation.

--- src/main/java/com/budgetme/enterprise/BudgetMeController.java
+++ src/main/java/com/budgetme/enterprise/BudgetMeController.java
@@ -3,8 +3,16 @@ package com.budgetme.enterprise;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller for handling web requests to the home page.
+ */
 @Controller
 public class BudgetMeController {
+    /**
+     * Handles requests to the root URL and returns the name of the start page view.
+     *
+     * @return the name of the start page view.
+     */
     @RequestMapping("/")
     public String index(){
         return "start";
