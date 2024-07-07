b2f0dc81e4815084b9a5070ac3934f34a1adc632
Cameron <cameron@Camerons-MacBook-Air.local>
2024-07-07 16:35:26 -0700

    starting working on html

--- src/main/java/com/budgetme/enterprise/BudgetMeController.java
+++ src/main/java/com/budgetme/enterprise/BudgetMeController.java
@@ -1,13 +1,36 @@
 package com.budgetme.enterprise;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BudgetMeController {
     @RequestMapping("/")
     public String index(){
         return "start";
+    }
+
+    @PostMapping("/create_user")
+    public String createUser(@RequestParam String Username, @RequestParam String Password){
+        return "dashboard";
+    }
+
+    @PostMapping("/login_user")
+    public String loginUser(@RequestParam String Username, @RequestParam String Password, Model model){
+        return "dashboard";
+    }
+
+    @GetMapping("/create_user")
+    public String showCreateUser(){
+        return "createUser";
+    }
+
+    @GetMapping("/login_user")
+    public String showLoginUser(){
+        return "loginUser";
     }
 }
