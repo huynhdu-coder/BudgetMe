1dfc719b4c4e58102c821b31fc46f8e8f83765b3
Dai Huynh <huynhdu@mail.uc.edu>
2024-07-28 02:02:12 -0400

    Fixing database connection

--- src/main/java/com/budgetme/enterprise/repository/UserRepository.java
+++ src/main/java/com/budgetme/enterprise/repository/UserRepository.java
@@ -3,15 +3,13 @@ package com.budgetme.enterprise.repository;
 import com.budgetme.enterprise.dao.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-    @Query("SELECT u FROM User u WHERE u.username = :username")
-    User findByUsername(@Param("username") String username);
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
-
--- src/main/java/com/budgetme/enterprise/service/UserService.java
+++ src/main/java/com/budgetme/enterprise/service/UserService.java
@@ -1,6 +1,5 @@
 package com.budgetme.enterprise.service;
 import com.budgetme.enterprise.dao.User;
-import com.budgetme.enterprise.repository.UserRepoCustom;
 import com.budgetme.enterprise.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,9 +8,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     @Autowired
-    private UserRepoCustom userRepositoryCustom;
+    private UserRepository userRepository;
 
-    public void createUser(String username, String password) {
-        userRepositoryCustom.insertUser(username, password);
+    public User createUser(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setIsLoggedIn(false);
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
